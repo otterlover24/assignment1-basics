3e035adbfb910425930187d67f5a134b4ade20dd
Tiansheng <otterlover24@gmail.com>
2025-08-06 18:06:17 +0800

    Rope test passed.

--- tests/adapters.py
+++ tests/adapters.py
@@ -20,6 +20,7 @@ from cs336_basics.linear import Linear
 from cs336_basics.embedding import Embedding
 from cs336_basics.rmsnorm import RMSNorm
 from cs336_basics.positionwise_feedforward import PositionwiseFeedForward
+from cs336_basics.rope import RotaryPositionalEmbedding
 
 def run_linear(
     d_in: int,
@@ -248,7 +249,9 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = RotaryPositionalEmbedding(theta, d_k, max_seq_len)
+    result = rope(in_query_or_key, token_positions)
+    return result
 
 
 def run_transformer_block(
